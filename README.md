dfc6b5aa933004345bfc9a0aca04279297a5a3e5
Tyrone <tyrone.cloma0219@gmail.com>
2022-12-03 11:10:41 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pixel Encounter Proxy
+
+A simple Deno server that serves as a proxy to the Pixel Encounter API to be
+able to use CORS
---
+++ server.ts
@@ -0,0 +1,35 @@
+import express from 'npm:express';
+import cors from 'npm:cors';
+
+const app = express();
+app.use(cors());
+
+const port = Deno.env.get('PORT');
+const baseUrl = 'https://app.pixelencounter.com/api';
+
+console.log(Deno.env.get('PORT'));
+
+app.get('/', (_req, res) => {
+   res.send({ message: 'Hello from Proxy! 👋' });
+});
+
+app.get('/json/random', async (_req, res) => {
+   const data = await fetch(`${baseUrl}/basic/monsters/random/json`);
+   res.send(await data.json());
+});
+
+app.get('/json/custom', async (req, res) => {
+   const {
+      primaryColor = 'FFFFFF',
+      secondaryColor = 'FFFFFF',
+      fillType = '0',
+   } = req.query;
+   const data = await fetch(
+      `${baseUrl}/basic/svgmonsters/json?primaryColor=%23${primaryColor}&secondaryColor=%23${secondaryColor}&fillType=${fillType}`
+   );
+   res.send(await data.json());
+});
+
+app.listen(port, () => {
+   console.log(`🪐 Server listening at http://localhost:${port} ✨`);
+});
