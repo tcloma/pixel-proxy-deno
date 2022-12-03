366dc4828946cab71343127c64d9dac6b1b7025b
Tyrone <tyrone.cloma0219@gmail.com>
2022-12-03 12:10:55 -0500

    updated to use dotenv

--- server.ts
+++ server.ts
@@ -1,15 +1,19 @@
+import 'https://deno.land/x/dotenv@v3.2.0/load.ts';
+import { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts';
 import {
    Application,
    Router,
    helpers,
 } from 'https://deno.land/x/oak@v11.1.0/mod.ts';
-import { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts';
 
 const app = new Application();
 const router = new Router();
 
 const appPort = Deno.env.get('PORT');
 const baseUrl = Deno.env.get('API_URL');
+
+app.use(router.routes());
+app.use(oakCors());
 
 router.get('/', (context) => {
    context.response.body = { message: 'Hello World!' };
@@ -28,8 +32,5 @@ router.get('/json/custom', async (context) => {
    context.response.body = await data.json();
 });
 
-app.use(router.routes());
-app.use(oakCors());
-
 console.log(`🪐 Server listening at http://localhost:${appPort} ✨`);
-await app.listen({ port: appPort });
+await app.listen({ port: parseInt(appPort as string) });
